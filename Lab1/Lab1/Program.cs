7a879135bebe61e3dbb520441f2ec114370caf1c
Oskar Olejnik <oolejnik@edu.cdv.pl>
2025-10-11 14:10:01 +0200

    My first commit

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,3 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Globalization;
 
-Console.WriteLine("Hello, World!");
+const int requiredAge = 18;
+const int beerRequiredAge = 16;
+const string accessDeniedMessage = "Musisz mieć 16 lat";
+const string accessAllowedMessage = "Witaj w sklepie";
+const string beerRestrictionMessage = "Musisz mieć 18 lat";
+
+
+
+
+// zadanie 1
+/*
+string password;
+do
+{
+    Console.Write("Podaj hasło: ");
+    password = Console.ReadLine();
+}
+while (password != "admin123");
+Console.WriteLine("Zalogowano pomyślnie!"); */
+
+
+//zadanie 2
+/*
+string number;
+int number1;
+do
+{
+    Console.Write("Podaj liczbę większą od zera: ");
+    number = Console.ReadLine();
+    number1 = int.Parse(number);
+}
+while (number1 <= 0);
+Console.WriteLine("Dobrze"); */
+
+//zadanie 3
+/*
+string[] towns = { "Wolsztyn", "Poznań", "Wrocław", "Leszno", "Zielona Góra" };
+
+foreach (string town in towns)
+{
+    Console.WriteLine(town);
+} */
+
