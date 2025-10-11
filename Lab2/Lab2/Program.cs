1f812c00ed56529540183ded1b430c04aef2251a
Oskar Olejnik <oolejnik@edu.cdv.pl>
2025-10-11 14:23:55 +0200

    My first commit

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,24 @@
+﻿//zadanie 4
+
+class KontoBankowe
+{
+    private double saldo;
+    public void Wplata(double kwota) { saldo += kwota; }
+    public void Wyplata(double kwota) { saldo -= kwota; }
+    public double PobierzSaldo() { return saldo; }
+}
+
+//zadanie 5
+class Zwierze
+{
+    public void Jedz() => Console.WriteLine("Zwierzę je");
+}
+class Pies : Zwierze
+{
+    public void Szczekaj() => Console.WriteLine("Hau hau!");
+}
+
+class Kot : Zwierze
+{
+    public void Miaucz() => Console.WriteLine("Miau!");
+}
